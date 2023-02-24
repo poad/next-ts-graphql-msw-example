94040f8739a7f37d0d2cd15d71c1f178d0f19e8a
Kenji Saito <ken-yo@mbr.nifty.com>
2023-02-24 23:47:23 +0900

    Bumps node modules

--- backend/bin/backend.ts
+++ backend/bin/backend.ts
@@ -6,21 +6,21 @@ import { BackendStack } from '../lib/backend-stack';
 const app = new cdk.App();
 
 const env = app.node.tryGetContext('env');
-const name = app.node.tryGetContext('appName')
+const name = app.node.tryGetContext('appName');
 if (!name) {
-  throw new Error('appName be not null')
+  throw new Error('appName be not null');
 }
 const appName = `${env ? `${env}-` : ''}${name}`;
 
 const parameterName = app.node.tryGetContext('parameterName');
 if (!parameterName) {
-  throw new Error('parameterName be not null')
+  throw new Error('parameterName be not null');
 }
 const bucketName = app.node.tryGetContext('s3bucketName');
 if (!bucketName) {
-  throw new Error('bucketName be not null')
+  throw new Error('bucketName be not null');
 }
- 
+
 new BackendStack(app, `${appName}-backend-stack`, {
   appName,
   parameterName,
