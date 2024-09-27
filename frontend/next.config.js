78a42129e2567ab2d42405be4907805c50cd6d1a
Kenji Saito <ken-yo@mbr.nifty.com>
2024-09-27 19:45:32 +0900

    Fix the turbopack with msw

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,13 +1,22 @@
 import withBundleAnalyzer from '@next/bundle-analyzer';
 
 /** @type {import('next').NextConfig} */
-const config =  {
+const config = {
   output: 'export',
   reactStrictMode: true,
   swcMinify: true,
   cleanDistDir: true,
   images: {
     unoptimized: true,
+  },
+  // Module not found: Can't resolve 'msw/browser' が解決するまで
+  experimental: {
+    turbo: {
+      resolveAlias: {
+        'msw/browser': 'node_modules/msw/lib/browser',
+        'msw/node': 'node_modules/msw/lib/node',
+      },
+    },
   },
 };
 
