b576949d33ee75f0cdc22e25c58cf1c139c51f79
Kenji Saito <ken-yo@mbr.nifty.com>
2025-04-10 16:26:06 +0900

    Bumps node modules

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -3,6 +3,8 @@ import { QueryPokemonListQuery } from '../gql/graphql';
 import { Inter } from 'next/font/google';
 import styles from '../styles/Home.module.css';
 
+import type { JSX } from 'react';
+
 const inter = Inter({ subsets: ['latin'] });
 
 const QUERY = gql`
